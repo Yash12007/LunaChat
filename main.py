9f893cd664ac61e7af148dc87635ef1119324f65
Yash Bodhe <106073201+Yash12007@users.noreply.github.com>
2023-08-30 15:33:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import webview
+
+webview.create_window(
+    "LunaChat",
+    url='https://yash12007.github.io/LunaChat',
+    width=1280,
+    height=720,
+    confirm_close=True
+)
+webview.start()
